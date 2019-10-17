e350b59cef4a36bababadf288798a5dae9d96438
jajan20 <jamie.jansen2@hva.nl>
2019-10-17 13:12:48 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/putio', (req, res) => {
+  console.log('PutIO:', req.body)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
