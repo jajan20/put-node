8cfa7dabe9eae84ae6005c8a16fd42cdadfdfde0
jajan20 <jamie.jansen2@hva.nl>
2019-10-17 14:43:02 +0200

    fixed issue

--- app.js
+++ app.js
@@ -3,24 +3,17 @@ const bodyParser = require('body-parser')
 const app = express()
  
 const port = process.env.PORT || 3000
-let activeClients = []
-
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.write('<h1>PutIO Service Test</h1>')
-  activeClients.push(res)
+  res.end()
 })
-
 
 app.post('/putio', (req, res) => {
   console.log('PutIO:', req.body)
-  activeClients.forEach(client => {
-    client.write(`<h2>${req.body.name}</h2>`)
-    client.end()
-  })
   res.end()
 })
 
