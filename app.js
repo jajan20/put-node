271f695290d3535182f6d75636d6f224f7321f31
jajan20 <jamie.jansen2@hva.nl>
2019-10-17 15:37:23 +0200

    added socketIO

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
-const express = require('express')
+const express = require('express'), app = express()
 const bodyParser = require('body-parser')
-const app = express()
- 
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.write('<h1>PutIO Service Test</h1>')
-  res.end()
+  console.log('GET:')
+  res.sendFile(__dirname + '/index.html')
 })
 
+io.on('connection', (socket) => {
+  socket.on('create data', function(data) {
+    console.log('IO Connection')
+    socket.emit('data created', data)
+  })
+})
+
+// app.post('/putio', (req, res) => {
+//   console.log('PutIO:', req.body)
+//   res.end()
+// })
+
 app.post('/putio', (req, res) => {
-  console.log('PutIO:', req.body)
-  res.end()
+  io.sockets.emit('data created', data)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
